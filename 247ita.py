3f67ae04bcfe75caed08256f33fb051389d38888
darietto17 <darietti93@gmail.com>
2026-05-07 23:07:24 +0200

    Update proxy link in 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-PRXLINK = "https://mvajro-proxydd.hf.space" # non mettere lo / finale
+PRXLINK = "https://darietto17-proxy.hf.space" # non mettere lo / finale
 
 
 # Costanti
